9942ed220ac6ef9832802990d3e32a312e05956a
黄朝阳 <1056375598@qq.com>
2023-11-30 17:23:05 +0800

    用于调试的py文件

---
+++ base/basePage_test.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/29 16:56
+# @Author  : 黄朝阳
+# @FileName: basePage_test  用于调试
+# @Software: PyCharm
+
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.support.wait import WebDriverWait
+from appium import webdriver
+from PIL import Image
+from selenium.common.exceptions import NoSuchElementException
+from common.initDriver import initsetupteardown
+
+
+class BasePage_test(initsetupteardown):
+    def __init__(self):
+        self.driver = self.init_driver()
+        self.driver.implicitly_wait(5)
+
+    def find_element_o_t(self,loc,timeout=30,poll=0.5):
+        """
+        根据定位获取单个元素
+        :param loc: 查找元素方式及属性值，元祖方式传递（By.ID,ID属性值）
+        :param timeout: 等待时长
+        :param poll: 查询频率
+        :return: 定位对象
+        """
+        try:
+            return WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_element(*loc))
+        except Exception as e:
+            return "查找元素异常"
+        #return self.driver.find_element(*loc)
+
+    def find_element_in_t(self,loc,timeout=30,poll=0.5):
+        """
+        根据定位获取单个元素
+        :param loc: 查找元素方式及属性值，元祖方式传递（By.ID,ID属性值）
+        :param timeout: 等待时长
+        :param poll: 查询频率
+        :return: 定位对象
+        """
+        return WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_element(*loc))
+
+    def click_element_t(self,loc,desc=''):
+        """
+        点击操作
+        :param loc: 查找元素方式及属性值，元祖方式传递（By.ID,ID属性值）
+        :return:
+        """
+        self.find_element_o_t(loc).click()
+
+    def input_text_t(self,loc,text,desc=''):
+        """
+        输入操作
+        :param loc: 查找元素方式及属性值，元祖方式传递（By.ID,ID属性值）
+        :param text: 输入的值
+        :return:
+        """
+        ele = self.find_element_o_t(loc)
+        ele.clear()
+        ele.send_keys(text)
+
+    def long_press_element_t(self,ele,duration=2000,desc=''):
+        """
+        长按操作
+        :param loc:
+        :return:
+        """
+        TouchAction(self.driver).long_press(el=ele,duration=duration)
+
+    def swipe_t(self,start_x,start_y,end_x,end_y,duration):
+        '''
+        滑动操作
+        :param start_x: 左滑的坐标
+        :param start_y: 右滑的坐标
+        :param end_x: 上滑的坐标
+        :param end_y: 下滑的坐标
+        :param duration: 等待时间
+        :return:
+        '''
+        windows_size = self.driver.get_window_size()
+        x = windows_size['width']
+        y = windows_size['height']
+        self.driver.swipe(start_x=x*start_x,
+                          start_y=y*start_y,
+                          end_x=x*end_x,
+                          end_y=y*end_y,
+                          duration=duration)
+
+
+    def get_screen_t(self, screen_name):
+        '''
+        全屏截图
+        :param screen_name:
+        :return:
+        '''
+        self.driver.get_screenshot_as_file("screen/" + screen_name)
+
+    def screenshot_t(self, partName, pictureName):
+        '''
+        共用方法：根据坐标区域精确截图
+        :param partName:
+        :param pictureName:
+        :return:
+        '''
+        box = (0, 0, 0, 0)
+        if partName == 'default':
+            box = (0, 210, 1110, 1430)  # 区域1
+        elif partName == 'hair':
+            box = (110, 270, 950, 1145)  # 区域2
+        self.driver.get_screenshot_as_file(
+            '../picture/screenshot.png')  # 路径和名称，图片是.png格式
+        image = Image.open('../picture/screenshot.png')
+        new_image = image.crop(box)
+        new_image.save(pictureName)  # 截取精确区域后的图片
+
---
+++ page/login_test.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/30 13:56
+# @Author  : 黄朝阳
+# @FileName: login_test  用于调试
+# @Software: PyCharm
+
+from appium import webdriver
+from selenium.webdriver.common.by import By
+from base.basePage_test import BasePage_test
+
+class initsetupteardown(BasePage_test):
+
+    # 勾选协议
+    check_xieyi = (By.ID, "cn.com.quanyou.attnd:id/deal_check_box")
+    # 切换[密码登录]
+    click_pulogin = (By.ID, "cn.com.quanyou.attnd:id/password_login_btn")
+    # 切换[刷脸登录]
+    face_login = (By.ID, "cn.com.quanyou.attnd:id/face_login_way")
+    # 输入密码输入框
+    password_input = (By.XPATH, "//android.widget.EditText[@text='请输入密码']")
+    # yoyo头像
+    user_avatar = (By.ID, "cn.com.quanyou.attnd:id/user_avatar")
+    # 输入账号
+    click_username = (By.ID, "cn.com.quanyou.attnd:id/username")
+    # 输入密码
+    click_password = (By.ID, "cn.com.quanyou.attnd:id/password")
+    # 登录
+    click_login = (By.ID, "cn.com.quanyou.attnd:id/login")
+
+    def init_driver(self):
+        desiredCaps = {}
+        #设备信息
+        desiredCaps["platformName"] = "Android"
+        #设备系统版本
+        desiredCaps["paltformVersion"] = "12"
+        #app包名
+        desiredCaps["appPackage"] = "cn.com.quanyou.attnd"
+        #app启动activity
+        desiredCaps["appActivity"] = "cn.com.quanyou.attnd.ui.SplashActivity"
+        #设备名字
+        desiredCaps["deviceName"] = "21091116AC"
+        #不清除应用数据
+        desiredCaps["noReset"] = True
+        #允许输入中文
+        desiredCaps["unicodeKeyboard"] = True
+        desiredCaps["resetKeyboard"] = True
+        #允许app获取相关权限
+        desiredCaps["autoGrantPermissions"]: True
+        desiredCaps["automationName"] = "UiAutomator2"
+        #修改超时时长，默认60s
+        desiredCaps["newCommandTimeout"] = 3600
+
+        #获取driver对象
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub",desiredCaps)
+        return self.driver
+
+    def login(self,username,password):
+        user_avatar_text = self.find_element_o_t(loc=(By.ID, "cn.com.quanyou.attnd:id/user_avatar"))
+        print(user_avatar_text)
+        if user_avatar_text == "查找元素异常":
+            self.input_text_t(self.click_username,username,'输入账号')
+            self.input_text_t(self.click_password,password,'输入密码')
+            self.click_element_t(self.check_xieyi,'勾选协议')
+            self.driver.implicitly_wait(3)
+            self.click_element_t(self.click_login,'点击登录')
+
+            # result = self.find_element_o_t(loc=(By.XPATH, "//android.widget.TextView[@text='账号密码错误!!']"))
+            result = self.find_element_o_t(loc=(By.XPATH, "//android.widget.TextView[@text='友信']")).text
+            print(result)
+            print(222222222222222222222222)
+            return result
+
+        else:
+            self.click_element_t(self.click_pulogin, '切换密码登录')
+            self.input_text_t(self.click_username,username,'输入账号')
+            self.input_text_t(self.click_password,password,'输入密码')
+            self.click_element_t(self.check_xieyi,'勾选协议')
+            self.driver.implicitly_wait(3)
+            self.click_element_t(self.click_login,'点击登录')
+
+            # result = self.find_element_o_t(loc=(By.XPATH, "//android.widget.TextView[@text='账号密码错误!!']"))
+            result = self.find_element_o_t(loc=(By.XPATH, "//android.widget.TextView[@text='友信']")).text
+            print(1111111111111111)
+            return result
+
+
+    def quit_driver(self):
+        self.driver.quit()
+
+
+
+if __name__ == '__main__':
+
+    loc = initsetupteardown()
+    login=loc.login(username="18728421687", password="12345678")
