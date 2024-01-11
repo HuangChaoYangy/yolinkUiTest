4d830aeff40659eb7912bf35495eb9feb105663f
黄朝阳 <1056375598@qq.com>
2024-01-11 13:34:31 +0800

    UI自动化发送文本消息

--- login_ui.py
+++ login_ui.py
@@ -6,8 +6,12 @@
 
 import selenium.webdriver.remote.webdriver
 
-from base.baseBage import BasePage
+
 from appium.webdriver.common.appiumby import By
+
+from base.baseBage import BasePage
+
+
 # from common.getYamlConfig import log
 class LoginPage1(BasePage):
 
@@ -43,21 +47,20 @@ class LoginPage1(BasePage):
         self.input_text(self.click_username, username, '输入账号')
         self.input_text(self.click_password, password, '输入密码')
         self.click_element(self.click_login, '点击登录')
-        print(self.find_element_o(self.xieyi_qux).text)
-        if self.find_element_o(self.xieyi_qux).text=="取消":
+        # print(self.find_element_o(self.xieyi_qux).text)
+        if self.is_element_exist(self.xieyi_qux):
             self.click_element(self.xieyi_queren)
             self.click_element(self.click_login, '点击登录')
-        else:
-            self.click_element(self.check_xieyi, '勾选协议')
-            self.click_element(self.click_login, '点击登录')
-            self.click_element(self.click_pop)
 
-        if self.find_element_o((By.XPATH,"//android.widget.TextView[@text='消息']"),timeout=3) == "查找元素异常":
-            # print(self.find_element_o(self.pop_text).text)
-            self.click_element(self.click_pop)
-            return "查找元素异常"
-        else:
+
+        if self.is_element_exist((By.XPATH,"//android.widget.TextView[@text='消息']")):
             return self.find_element_o((By.XPATH, "//android.widget.TextView[@text='消息']")).text
+            # print(self.find_element_o(self.pop_text).text)
+        else:
+            result = self.find_element_o(self.pop_text).text
+            self.click_element(self.click_pop)
+            print(result)
+            return result
 
 
     def swith_todo(self,username,password):
@@ -75,15 +78,15 @@ class LoginPage1(BasePage):
         # print('----------------------------------')
         # print(self.find_element_o(self.pop_text))
         # print('----------------------------------')
-        if self.find_element_o((By.XPATH,"//android.widget.TextView[@text='消息']"),timeout=3) == "查找元素异常":
+        if self.is_element_exist((By.XPATH,"//android.widget.TextView[@text='消息']")):
+            return self.find_element_o((By.XPATH, "//android.widget.TextView[@text='消息']")).text
+        else:
             self.click_element(self.click_pop)
             self.click_element(self.click_username)
             self.find_element_o(self.click_username).clear()
             self.click_element(self.click_password)
             self.find_element_o(self.click_password).clear()
             return "查找元素异常"
-        else:
-            return self.find_element_o((By.XPATH, "//android.widget.TextView[@text='消息']")).text
 
     def quit_login(self):
         """
@@ -97,19 +100,27 @@ class LoginPage1(BasePage):
     def login(self,username,password):
         if self.find_element_o((By.XPATH,"//android.widget.TextView[@text='消息']"),timeout=3)!="消息":
             print('进入')
-            if self.find_element_o(self.click_pulogin, '密码登录') == '查找元素异常':
+            if self.is_element_exist(self.click_pulogin):
+                    # (self.find_element_o(self.click_pulogin, '密码登录') != '查找元素异常'):
+                print('进入刷脸')
                 result =self.swith_todo(username,password)
+                self.quit_login()
                 return result
                 # log().info('--进入打印---')
             else:
-                self.swith_to(username,password)
-                self.quit_login()
+                print('密码登录页面')
+                result = self.swith_to(username,password)
+                return result
                 # log().info('self.click_login')
         else:
+            print('退出')
             self.quit_login()
             result =self.swith_to(username,password)
             return result
 
 
+if __name__ == '__main__':
 
+    loginObject = LoginPage1(BasePage)
+    loginObject.login(username='18728421687',password='12345678')
 
